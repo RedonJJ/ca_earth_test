3755c620124e6bcd4111ca128b3d459aed38d164
RedonJJ <jjredon.dev@gmail.com>
2022-11-22 03:52:40 +0100

    Exercice de l'Alphabet

---
+++ Alphabet.rb
@@ -0,0 +1,13 @@
+#Créer un programme qui affiche l'alphabet en lettres minuscules suivi d'un retour à la ligne.
+#Le programme doit utiliser une boucle.
+
+alphabet = ('a'..'z').to_a
+lettre = ""
+
+for lettre in alphabet
+    print "#{lettre}" 
+end 
+
+print "\n"
+
+#Fin de l'exercice.
