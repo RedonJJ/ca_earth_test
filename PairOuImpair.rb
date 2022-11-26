65f305ea619149578e360af499197dbbcd92d9e7
RedonJJ <jjredon.dev@gmail.com>
2022-11-26 14:00:30 +0100

    ajout de l'exercice PairOuImpair non terminé

---
+++ PairOuImpair.rb
@@ -0,0 +1,13 @@
+#Créer un programme qui permet de déterminer si l'argument donné est un entier pair ou impair, l'exercice doit aussi gérer les nombres négatifs.
+
+nombre = 
+
+if nombre == nombre.to_i
+    if(nombre % 2) == 0
+    puts "Le numéro #{nombre} est pair."
+else
+    puts "Le numéro #{nombre} est impair."
+end
+else
+    puts "Tu ne me la mettras pas à l'envers"
+end
