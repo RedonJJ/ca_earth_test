c12779b88e8ad806f9177f73416007c3e1088900
RedonJJ <jjredon.dev@gmail.com>
2022-12-01 21:09:35 +0100

    ajout de condition empêche le dividende d'être inférieur au diviseur Terre05.rb

--- Terre05.rb
+++ Terre05.rb
@@ -5,9 +5,11 @@ def division()
     x = gets.chomp.to_i
     print "Choisissez le diviseur: "
     y = gets.chomp.to_i
-    if x == 0 || y == 0
+    if x == 0 || y == 0 then
         puts "Erreur, le dividende et le diviseur ne peuvent pas être égale à 0."
-    else 
+    elsif x < y then
+        puts "Erreur, le dividende ne peut pas être plus petit que le diviseur."
+    else
         puts "#{x} divisé par #{y} = #{x / y}"
         puts "Il reste #{x % y}"
     end
